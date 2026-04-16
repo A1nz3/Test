a50b6dd16fc1575ee55f6bd7989b1179777cae2f
Student <student@ib.polsl.pl>
2026-04-16 14:34:05 +0200

    dodano pole trojkata

---
+++ github_lab.m
@@ -0,0 +1,15 @@
+%%Triangle
+
+a = input('Podaj długość boku a: ');
+b = input('Podaj długość boku b: ');
+c = input('Podaj długość boku c: ');
+
+if (a + b > c) && (a + c > b) && (b + c > a)
+    p = (a + b + c) / 2;
+    P = sqrt(p * (p - a) * (p - b) * (p - c));
+    
+    fprintf('Pole trójkąta = %.2f\n', P);
+else
+    disp('Z podanych boków nie można utworzyć trójkąta!');
+end
+
